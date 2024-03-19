1cd4f644fb14b61752e36a897e80c25ed5052611
llldmitrilll <drdmitri@yandex.ru>
2024-03-19 11:41:04 +0300

    redux-slices_reduxjs/toolkit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Layout from "./components/Layout/Layout";
 import Cart from "./components/Cart/Cart";
 import Products from "./components/Shop/Products";
+import { useSelector } from "react-redux";
+import { Fragment, useEffect } from "react";
 
 function App() {
+  const isCartVisible = useSelector((state) => state.main.isCartVisible);
+  const cart = useSelector((state) => state.cart);
+
   return (
-    <Layout>
-      <Cart />
-      <Products />
-    </Layout>
+    <Fragment>
+      <Layout>
+        {isCartVisible && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
+
   );
 }
 
